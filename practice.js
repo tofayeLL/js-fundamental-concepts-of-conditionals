b5a664b0d084b48c897b6080b8ea679a38b302ad
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-28 01:12:59 +0600

    finished practice task two about bmi calculator

--- practice.js
+++ practice.js
@@ -2,10 +2,37 @@
 
 /* -------practice task one------- */
 
-var burgerPrice = 100;
+/* var burgerPrice = 100;
 if(burgerPrice > 500){
     console.log('you will get free coke');
 }
 else{
     console.log('you have to pay 30 taka for coke');
+} */
+
+
+/* -------practice task two------- */
+/* bmi (body mass index) */
+/* var weight = 50;
+var height = 1.5;
+var bmi = weight / (height ** 2); */ /* Math.pow(height, 2); we can use this for height square */
+ 
+var bmi = 30;
+console.log('your bmi is =',bmi);
+
+if(bmi){
+    if(bmi < 18.5){
+        console.log('so you are underweight');
+    }
+    else if(bmi >= 18.5  &&  bmi <= 24.9 ){
+        console.log('so your weight is normal');
+    }
+    else if(bmi >= 25  &&  bmi <= 29.9 ){
+        console.log('so you are overweight');
+    }
+    else{
+        console.log('you are obese or fatty');
+    }
 }
+
+
