35362cc5bd9220df29257692316d346febdcf9f7
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-28 00:38:31 +0600

    finished practice task one

--- practice.js
+++ practice.js
@@ -1,2 +1,11 @@
 
 
+/* -------practice task one------- */
+
+var burgerPrice = 100;
+if(burgerPrice > 500){
+    console.log('you will get free coke');
+}
+else{
+    console.log('you have to pay 30 taka for coke');
+}
